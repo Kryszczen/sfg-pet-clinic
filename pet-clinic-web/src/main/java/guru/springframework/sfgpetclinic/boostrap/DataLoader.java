aded2398be0d394dc2f7dca214009583fb6f34fd
Jacek <jcchrzescijanek@gmail.com>
2020-05-01 11:35:21 +0200

    Refactoring AbstractMapService to autoupdate IDs. closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/boostrap/DataLoader.java
@@ -25,28 +25,24 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Smith");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Michael");
         owner2.setLastName("Jakckson");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Eddie");
         vet1.setLastName("Murphy");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Robert");
         vet2.setLastName("Downey");
 
